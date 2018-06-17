4841e46a5bb2255869b12ef079ef6813f563c852
Solomiia Demkiv <solomiia.demkiv@gmail.com>
2018-06-17 23:12:00 +0300

    Remove template components

--- ImageSearchEngine/ClientApp/components/Layout.tsx
+++ ImageSearchEngine/ClientApp/components/Layout.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { NavMenu } from './NavMenu';
 
 export interface LayoutProps {
     children?: React.ReactNode;
@@ -8,14 +7,7 @@ export interface LayoutProps {
 export class Layout extends React.Component<LayoutProps, {}> {
     public render() {
         return <div className='container-fluid'>
-            <div className='row'>
-                <div className='col-sm-3'>
-                    <NavMenu />
-                </div>
-                <div className='col-sm-9'>
-                    { this.props.children }
-                </div>
-            </div>
+			{this.props.children}
         </div>;
     }
 }
